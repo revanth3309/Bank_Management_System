877fdd806a3bf644ecf275401978abcdd9c08f1f
revanth3309 <revanth3309@gmail.com>
2025-06-04 19:49:34 +0530

    Added mini statement class

--- src/bank/management/system/main_Class.java
+++ src/bank/management/system/main_Class.java
@@ -99,6 +99,8 @@ public class main_Class extends JFrame implements ActionListener {
         }else if (e.getSource()==b5) {
             new Pin(pin);
             setVisible(false);
+        }else if (e.getSource()==b4) {
+            new mini(pin);
         }
     }
 
